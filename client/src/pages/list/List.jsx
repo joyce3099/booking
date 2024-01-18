55c5a4abba1e9a72c18b53e3151e877a577f328a
joyce3099 <132931386+joyce3099@users.noreply.github.com>
2024-01-18 13:17:10 -0800

    Update List.jsx

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -20,7 +20,7 @@ const List =() =>{
     const [min,setMin] = useState(undefined)
     const [max,setMax] = useState(undefined)
 
-    const {data,loading,error,reFetch} = useFetch(`https://booking-backend-phi.vercel.app/api/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`)
+    const {data,loading,error,reFetch} = useFetch(`https://booking-server-ppve.onrender.com/api/hotels?city=${destination}&min=${min || 0}&max=${max || 999}`)
     
     const handleClick = () => {
         reFetch();
